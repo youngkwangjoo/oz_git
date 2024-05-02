d4f6225887fc21fa77e4ec2daa0a22e4fdc26fd5
youngkwangjoo <dudrknd1642@gmail.com>
2024-05-02 11:17:23 +0900

    flask task

---
+++ part4/models.py
@@ -0,0 +1,24 @@
+#model -> 테이블 생성
+#게시글 - board
+#유저 - user 
+
+from db import db
+
+class User(db.Model):
+    __tablename__ = "users"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), unique=True, nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    boards = db.relationship('Board', back_populates='author', lazy='dynamic')
+
+	# address = db.Column(db.String(120), unique=True, nullable=False)  # 추가된 필드
+
+class Board(db.Model):
+    __tablename__ = "boards"
+
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(100), nullable=False)
+    content = db.Column(db.String(200))
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    author = db.relationship('User', back_populates='boards')
